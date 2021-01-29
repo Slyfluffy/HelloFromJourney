68fcbefb106637b950cbbb0b5edd390b700b5fbe
Tayde Luevano <tayde_luevano@yahoo.com>
2021-01-28 18:37:00 -0800

    Said hello to Journey.

--- src/journey/Main.java
+++ src/journey/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Journey");
+	System.out.println("Hello Journey! Tayde A. Luevano, at your service. *Makes a bow*");
     }
 }
